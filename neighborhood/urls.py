7d399971b7c923b6eb8f0d2c7a19fcd2020813c6
tomito26 <tommybwah@gmail.com>
2020-10-30 23:06:54 +0300

    linked the registration file to the projects.url and the admin page

--- neighborhood/urls.py
+++ neighborhood/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
 """
 from os import name
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from users import views as user_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.register,name='register'),
+    path('',include('users.urls'))
     
 ]
