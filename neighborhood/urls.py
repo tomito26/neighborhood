3cb250261457ac6e384a1489b478113b4d4c27f4
tomito26 <tommybwah@gmail.com>
2020-10-30 22:26:23 +0300

    created a form validation view

--- neighborhood/urls.py
+++ neighborhood/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from os import name
 from django.contrib import admin
 from django.urls import path
+from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', user_views.register,name='register'),
+    
 ]
