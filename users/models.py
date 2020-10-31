52f44519eb8fe99d9fc3d555bd3f9b01ab432dff
tomito26 <tommybwah@gmail.com>
2020-10-31 16:18:29 +0300

    added signal file for creating instance profiles

--- users/models.py
+++ users/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+from django.db.models.fields import TextField
+
+# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    image=CloudinaryField('profile-photo',null=True, transformation=[{'width':300, 'height':300}])
+    bio=models.TextField(null=True)
+
+    
+    def __str__(self):
+        return f'{self.user.username} Profile'
