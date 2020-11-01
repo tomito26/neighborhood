08a37a005ce5e1eb70c2fdfd4761c52ddaaa663f
tomito26 <tommybwah@gmail.com>
2020-11-01 22:20:40 +0300

    created delete,update and create post class views and also templates

--- estate/urls.py
+++ estate/urls.py
@@ -1,10 +1,12 @@
-from estate.views import BusinessCreateView, BusinessDetailView, BusinessListView
+from estate.views import BusinessCreateView, BusinessDeleteView, BusinessDetailView, BusinessListView, BusinessUpdateView
 from django.conf.urls import url
 from django.urls import path
-from estate import views
+from . import views
 
 urlpatterns = [
     path('',BusinessListView.as_view(),name='home'),
     path('business/<int:pk>/',BusinessDetailView.as_view(), name='business-detail'),
     path('business/new/', BusinessCreateView.as_view(), name='business-create'),
+    path('business/<int:pk>/update/',BusinessUpdateView.as_view(), name='business-update'),
+    path('business/<int:pk>/delete/',BusinessDeleteView.as_view(),name='business-delete')
 ]
--- estate/views.py
+++ estate/views.py
@@ -1,23 +1,51 @@
 from django.shortcuts import render
 from .models import Business
-from django.views.generic  import ListView,DetailView,CreateView
-
+from django.views.generic  import ListView,DetailView,CreateView,UpdateView,DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
 # Create your views here.
 
-class BusinessListView(ListView):
+class BusinessListView(LoginRequiredMixin,ListView):
     model = Business
     template_name= 'estate/home.html'
     context_object_name = 'businesses'
-    
-    
-class BusinessDetailView(DetailView):
+
+
+class BusinessDetailView(LoginRequiredMixin,DetailView):
     model = Business
-    
-class BusinessCreateView(CreateView):
+
+class BusinessCreateView(LoginRequiredMixin,CreateView):
     model = Business
     fields = ['business_name','email','business_image']
-    
+
     def form_valid(self, form):
         form.instance.business_owner = self.request.user
         return super().form_valid(form)
-    
+
+
+class BusinessUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
+    model = Business
+    fields = ['business_name', 'email', 'business_image']
+
+    def form_valid(self, form):
+        form.instance.business_owner = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        business = self.get_object()
+
+        if self.request.user == business.business_owner:
+            return True
+        return False
+
+
+class BusinessDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
+    model = Business
+    success_url = '/'
+
+    def test_func(self):
+        business = self.get_object()
+        
+
+        if self.request.user == business.business_owner:
+            return True
+        return False
