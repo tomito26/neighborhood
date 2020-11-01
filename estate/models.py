57413c4c8fa847474102be767463100a1f66f456
tomito26 <tommybwah@gmail.com>
2020-11-01 09:04:46 +0300

    added a new  model on the estate app

--- estate/models.py
+++ estate/models.py
@@ -1,1 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Business(models.Model):
+    business_name =  models.CharField(max_length=100)
+    user = models.ForeignKey(User,on_delete=models.CASCADE, null=True)
+    business_email = models.EmailField()
+    
+    
+    def __str__(self):
+      return self.business_name
--- users/models.py
+++ users/models.py
@@ -1,6 +1,4 @@
 from django.db import models
-
-# Create your models here.
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 from django.db.models.fields import TextField
