f30b7358456dac9184cd2c3211f90d014c08c6cf
tomito26 <tommybwah@gmail.com>
2020-11-02 15:50:34 +0300

    linked the creat post to the post_list page

--- estate/urls.py
+++ estate/urls.py
@@ -13,5 +13,5 @@ urlpatterns = [
     path('post/new/',PostCreateView.as_view(),name='post-create'),
     path('post/<int:pk>/',PostDetailVew.as_view(), name='post-detail'),
     path('post/<int:pk>/update/',PostUpdateView.as_view(), name='post-update'),
-     path('post/<int:pk>/delete/',PostDeleteView.as_view(), name='post-delete'),
+    path('post/<int:pk>/delete/',PostDeleteView.as_view(), name='post-delete'),
 ]
--- estate/views.py
+++ estate/views.py
@@ -56,6 +56,7 @@ class PostListView(LoginRequiredMixin,ListView):
     model = Post
     template_name = 'estate/post_list.html'
     context_object_name = 'posts'
+    ordering = '-date_posted'
 
 class PostCreateView(CreateView):
     model = Post
